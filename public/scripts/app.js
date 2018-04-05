f73c82746f62d9b0ae5dbebd78130171f725fd9b
Brian Holden <bkavuh@gmail.com>
2018-04-05 20:02:23 +0000

    update for merging with craig

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,6 +6,7 @@ $(() => {
     for(user of users) {
       $("<div>").text(user.name).appendTo($("body"));
       $("<div>").text(user.email).appendTo($("body"));
+      $("<div>").text(user.user_score_goofspiel).appendTo($("body"));
     }
   });;
 });
