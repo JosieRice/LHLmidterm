b7f946361e36b92c12c655a2742774d89b5a0f75
Craig Rice <craigricetong@gmail.com>
2018-04-06 01:54:27 +0000

    added css card pack and did styles for game board

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,14 @@
 
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/users"
-//   }).done((users) => {
-//     for(user of users) {
-//       $("<div>").text(user.name).appendTo($("body"));
-//       $("<div>").text(user.email).appendTo($("body"));
-//       $("<div>").text(user.user_score_goofspiel).appendTo($("body"));
-//     }
-//   });;
-// });
+$(() => {
+  $.ajax({
+    method: "GET",
+    url: "/api/users"
+  }).done((users) => {
+    for(user of users) {
+      $("<div>").text(user.name).appendTo($("#leader-board"));
+      $("<div>").text(user.email).appendTo($("#leader-board"));
+      $("<div>").text(user.user_score_goofspiel).appendTo($("#leader-board"));
+    }
+  });;
+});
 
